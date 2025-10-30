29cf90f4f3b6c76f6277aafa7a2d28008045bec8
Fzc-s <ziohio91@gmail.com>
2025-10-30 20:54:10 +0300

    c

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import create_app, db
+from flask_migrate import upgrade  # Если используете миграции
 
 app = create_app()
 
-
 with app.app_context():
-    db.create_all() 
+    upgrade()  # Применит миграции
 
 if __name__ == '__main__':
     app.run()
