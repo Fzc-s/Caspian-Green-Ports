9980dceeb13bc7c84224d353f0cb7ef3e247f891
Fzc-s <ziohio91@gmail.com>
2025-10-31 07:27:01 +0300

    UwU

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from app import create_app, db
-from flask_migrate import upgrade  # Если используете миграции
+from app.models import User, Port, Report
 
 app = create_app()
 
 with app.app_context():
-    upgrade()  # Применит миграции
+    if not User.query.first():
+        # Seed код
+        user = User(username='admin', role='admin')
+        user.set_password('adminpass')
+        db.session.add(user)
+        db.session.commit()
 
 if __name__ == '__main__':
     app.run()
